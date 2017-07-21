e0b753d6c2432d95e4045e98672307f68098f872
zhangdd <894566315@qq.com>
2017-07-21 16:58:04 +0800

    [bug]测试类

---
+++ meterservice-api/src/test/java/com/ry/taxi/web/order/CreateOrderTest.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.ry.taxi.web.order;
+
+import java.util.Date;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ry.taxi.RyMeterServiceApplicationTest;
+import com.ry.taxi.order.domain.OpTaxiOrder;
+import com.ry.taxi.order.mapper.OrderTestMapper;
+import com.szyciov.util.StringUtil;
+
+/**
+ * @Title:CreateOrderTest.java
+ * @Package com.ry.taxi.order.web
+ * @Description
+ * @author zhangdd
+ * @date 2017年7月21日 上午10:44:51
+ * @version 
+ *
+ * @Copyrigth  版权所有 (C) 2017 广州讯心信息科技有限公司.
+ */
+public class CreateOrderTest extends RyMeterServiceApplicationTest  {
+	
+	@Autowired
+	private OrderTestMapper orderTestMapper;
+	
+	@Test
+	public void createOpOrder(){
+		OpTaxiOrder order = new OpTaxiOrder();
+		order.setOrderno("CGT" + StringUtil.formatDate(new Date(), "yyMMddHHss"));
+		order.setUserid("477B13B9-1629-4A2A-8896-9307317CF6D8");
+		order.setUsetype("1");//因私用车
+		order.setOrdertype("4");//出租车
+		order.setPaymentmethod("1");//线下支付
+		
+		order.setPassengers("乘客-测试号");
+		order.setPassengerphone("18620291125");
+		
+		order.setOncity("广州市");
+		order.setOnaddress("广州市燕岭路633号");
+		order.setOnaddrlat(23.1764890000);
+		order.setOnaddrlng(113.3490850000);
+		
+		order.setOffcity("广州市");
+		order.setOffaddress("广东省广州市天河区花城大道53号");
+		order.setOffaddrlat(23.1257230000);
+		order.setOffaddrlng(113.3334270000);
+		
+		order.setEstimatedmileage(11);
+		order.setEstimatedtime(18);
+		order.setEstimatedcost(33);
+		order.setPricecopy("");
+		
+		order.setPaymentstatus("6");
+		order.setReviewstatus("0");
+		order.setUserhidden("1");
+		order.setDriverid("");
+		
+		order.setOrderstatus("1");
+		order.setThirdorderno(order.getOrderno());
+        
+		orderTestMapper.insertOpTaxiOrder(order);
+		
+	}
+	
+
+}
---
+++ meterservice-api/src/test/java/com/ry/taxi/web/order/OrderTakingTest.java
@@ -0,0 +1,66 @@
+package com.ry.taxi.web.order;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+
+
+import com.ry.taxi.web.BaseWebTest;
+import com.xunxintech.ruyue.coach.encryption.algorithm.DESUtils;
+import com.xunxintech.ruyue.coach.encryption.algorithm.MD5;
+
+
+
+/**
+ * 
+  * @Title: BaseWebTest.java 
+  * @Package com.xunxintech.ruyue.coach.web 
+  * @Description  TODO
+  * @author  XZF
+  * @date 2017年1月3日 下午3:56:55 
+  * @version   
+  *
+  * @Copyrigth  版权所有 (C) 2016 广州讯心信息科技有限公司.
+  *
+ */
+public class OrderTakingTest extends BaseWebTest {
+	
+	@Autowired
+	protected TestRestTemplate testRest;
+
+	private String  Key = "12345678";
+	
+	public String takeArgs() throws JSONException{
+		 JSONObject argJson = new JSONObject();
+		 argJson.put("CertNum", "100804");
+		 argJson.put("Mobile", "18620291125");
+		 argJson.put("PlateNum", "4YC46");
+		 argJson.put("OrderNum", "CGT1707211610");
+		 argJson.put("ResponseType", 1);
+		 return argJson.toString();
+	}
+    
+	
+    /*
+     * 司机应邀通知
+     */
+	@Test
+	public void OrderTakingTest() throws JSONException{
+		String argJson = takeArgs();
+		String sign =  MD5.MD5Encode(DESUtils.encode(Key,  argJson, Key), "utf-8");
+		String url ="/RyTaxi/Management/func?Cmd=DriverTakeOrder&Key=" + Key + "&UserId=CGI"+"&Args={json}"+"&Sign="+sign;
+		
+		HttpEntity<String> request = new HttpEntity<String>(argJson, null);
+		ResponseEntity<String> responseEntity = this.testRest.postForEntity(url, request, String.class,argJson);		
+		String data = responseEntity.getBody();
+		System.out.println(data);
+        
+	}
+
+
+}
